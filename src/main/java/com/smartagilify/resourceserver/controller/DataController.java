d8d93b95b7e62ccad7067fa1e444b363a08efc06
dehghani-reza <dehghan7447@gmail.com>
2024-03-26 11:04:12 +0330

    feat: add method security define by scope

--- src/main/java/com/smartagilify/resourceserver/controller/DataController.java
+++ src/main/java/com/smartagilify/resourceserver/controller/DataController.java
@@ -2,18 +2,27 @@ package com.smartagilify.resourceserver.controller;
 
 import java.util.Map;
 
+import com.smartagilify.resourceserver.service.DataService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/data")
 public class DataController {
 
+	private final DataService dataService;
+
 	@GetMapping
 	public Map<String, String> getData(@AuthenticationPrincipal Jwt jwt) {
-		return Map.of("message", "hello-> " + jwt.getSubject());
+		log.info("username(subjective) that using get data api: " + jwt.getSubject());
+		return dataService.getDate();
 	}
 }
